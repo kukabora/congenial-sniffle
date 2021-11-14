091236439916273c920cbe1a2c3ec5c13cc13094
Alex <youdoit@mail.ru>
2021-11-14 22:54:39 +0600

    Added new template

--- OlimpBet/BetApplication/models.py
+++ OlimpBet/BetApplication/models.py
@@ -7,7 +7,6 @@ from django.contrib.auth.models import User
 class CustomUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=50, null=False)
-
     balance = models.IntegerField(default=0)
 
 
